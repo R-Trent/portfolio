d8918a3a63cc38481f1a89930b0305c13918f80c
Ryan Trent <rtrent.dev@gmail.com>
2022-10-14 15:09:28 -0700

    Fix Safari Support (Download on App Store Button)

--- components/buttons/AppStoreButton.tsx
+++ components/buttons/AppStoreButton.tsx
@@ -1,14 +1,20 @@
 import Link from '../Link'
+import Image from '../Image'
 
 const AppStoreButton = ({ applink }) => {
   return (
     <div style={{ minWidth: '140px', maxWidth: '200px' }}>
-      <Link href={applink} className={`flex h-full items-center items-stretch rounded-xl`}>
-        <img
-          src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1665532800&h=787ae4514f03be53939eece7de396b19"
+      <Link href={applink} className={`flex h-full items-stretch rounded-xl`}>
+        <Image
           alt="Download on the App Store"
-          object-fit="contain"
-        ></img>
+          src="/static/glyphs/appstore.svg"
+          className="object-cover object-center dark:invert"
+          width={140}
+          height={48}
+          placeholder="blur"
+          blurDataURL="/static/glyphs/appstore.svg"
+          objectFit="fill"
+        />
       </Link>
     </div>
   )
